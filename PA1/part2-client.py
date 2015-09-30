d35afbc3da2fd8c36165bada996ea5cc78835f45
= <=>
2015-09-30 18:40:50 -0400

    more changes to PA1

--- PA1/part2-client.py
+++ PA1/part2-client.py
@@ -1,7 +1,14 @@
-import sys, socket
+#!/usr/bin/python
+import sys, socket, time
 
 class ClientSocket:
-    def __init__(self, host, port):
+    def __init__(self, host, port, mt, num_p, msg_size, s_delay):
+        # Initialize variables
+        self.measurement_type = mt
+        self.number_of_probes = num_p
+        self.msg_size = msg_size
+        self.server_delay = s_delay
+
         # Create the socket and connect to the host
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,39 +20,43 @@ class ClientSocket:
             print 'The connection has timed out'
             sys.exit(1)
 
-    def setup_phase(self, **kwargs):
-        msg = construct_msg('s', kwargs)
+    def setup_phase(self):
+        msg = self.construct_msg('s')
         sent = self.socket.send(msg)  # send a message to the server
-        if sent != len(msg):
-            print 'Entire message was not recieved by server'
-        self.get_response() # Wait for the servers response
+        response = self.socket.recv(1024)
+        if response != '200 OK: Ready':
+            print response
+            sys.exit(1)
+        self.measurement_phase()
 
-    def get_response(self):
-        # Get the response from the server and print it
-        msg = self.socket.recv(2048)
+    def measurement_phase(self):
+        times = list()
+        for i in range(0, int(self.number_of_probes)):
+            msg = self.construct_msg('m', i)
+            start_time = time.time()
+            self.socket.send(msg)
+            ack = self.socket.recieve(len(msg))
+            stop_time = time.time()
+            if not ack:
+                print 'Server not responding'
+                break
+            times.append(stop_time - start_time)
+        print times
 
-        # Echo the message and exit
-        print msg
-        self.socket.close()
-        sys.exit(1)
-
-    @classmethod
-    def construct_msg(phase, **kwargs):
+    def construct_msg(self, phase, seq=0):
+        # This function will construct messages on behalf of the client
         if phase == 's':
-            measurement_type = kwargs.get('measurement_type')
-            number_of_probes = kwargs.get('number_of_probes')
-            msg_size = kwargs.get('msg_size')
-            server_delay = kwargs.get('server_delay') or 0
-            if not all(measurement_type, number_of_probes, msg_size, server_delay):
-                print 'Missing parameters, please try again'
-                sys.exit(1)
-            return 's {} {} {} {}\n'.format(measurement_type, number_of_probes, msg_size, server_delay)
+            return 's {} {} {} {}\n'.format(self.measurement_type, self.number_of_probes, self.msg_size, self.server_delay)
+        if phase == 'm':
+            return 'm {} {}\n'.format(seq, 'a'*int(self.msg_size))
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print "Please enter the host and port number"
+    if len(sys.argv) != 6:
+        print "Please enter the host, port, measurement type, number of probes, and message size (optional server delay)"
         sys.exit(1)
-    host, port = sys.argv[1], int(sys.argv[2])
-    client = ClientSocket(host, port)
-    client.send_msg()
+    host, port, mt, num_p, msg_size, server_delay = sys.argv[1], int(sys.argv[2]), sys.argv[3], sys.argv[4], sys.argv[5], 0
+    if len(sys.argv) > 6:
+        server_delay = sys.argv[5]
+    client = ClientSocket(host, port, mt, num_p, msg_size, server_delay)
+    client.setup_phase()
--- PA1/part2-server.py
+++ PA1/part2-server.py
@@ -1,10 +1,22 @@
-import socket, sys
+import socket, sys, time
+
+class Setup404Exception(Exception):
+    def __init__(self):
+        self.msg = "404 ERROR: Invalid Connection Setup Message"
+
+class MP404Exception(Exception):
+    def __init__(self):
+        self.msg = "404 ERROR: Invalid Measurement Message"
 
 class ServerSocket:
     def __init__(self, port):
         # Initialize the class variables
-        self.message = None
+        self.measurement_type = None
+        self.number_of_probes = None
+        self.msg_size = 0
+        self.server_delay = 0
         self.client_socket = None
+
         # Create the server
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,7 +26,7 @@ class ServerSocket:
             print 'Could not establish connection of port {}'.format(port)
             sys.exit(1)
 
-    def listen(self, max_connections=5):
+    def listen(self, max_connections=1):
         # Listen to incoming connections
         self.socket.listen(max_connections)
 
@@ -24,26 +36,63 @@ class ServerSocket:
             print '{} has connected'.format(self.client_address)
             self.client_socket.settimeout(5)
 
-    def get_message(self):
-        # Get a message from an open port of up to 2048 bytes
+    def setup_phase(self):
+        # Recieve the setup message
         try:
-            self.message = self.client_socket.recv(2048)
-            self.send_message()
+            setup_msg = self.client_socket.recv(1024)
+            response = self.parse_msg(setup_msg)
+            self.send_message(response)
+            # If the message is correct, enter the measurement_phase
+            self.measurement_phase()
         except socket.timeout as msg:
             # This means that the client has not sent a valid message
-            print 'This socket has timed out recieving the message'
-            # Tell the client his message was inavlid and close the connection
-            self.client_socket.send('You did not send me a valid message')
+            print 'Connection timeout'
+            self.client_socket.close()
+        except Setup404Exception as e:
+            # Client sent a malformed message
+            self.client_socket.send(e.msg)
             self.client_socket.close()
 
-    def send_message(self):
+    def measurement_phase(self):
+        try:
+            for seq in range(0, int(self.number_of_probes)):
+                msg = self.client_socket.recv(int(self.msg_size) + 1024)  # Need a buffer at least as big as the payload
+                self.parse_msg(msg, seq)
+                time.sleep(self.server_delay)  # Wait the specified amount
+                self.send_message(msg)
+        except MP404Exception as e:
+            self.send_message(e.msg)
+            self.client_socket.close()
+
+    def send_message(self, msg):
         # Send the message back to the client
         try:
-            self.client_socket.send(self.message)
+            self.client_socket.send(msg)
         except socket.timeout as msg:
             print 'This socket timed out'
-        finally:
             self.client_socket.close()
+
+    def parse_msg(self, msg, seq=0):
+        msg = msg.split(' ')  # Split the message by whitespaces
+        if len(msg) == 0: raise Setup404Exception
+        # Parse the setup message
+        if msg[0] == 's':
+            self.validate_message('s', msg)
+            # Pull out the information from the message
+            self.measurement_type, self.number_of_probes, self.msg_size, self.server_delay = msg[1:]
+            return "200 OK: Ready"
+        if msg[0] == 'm':
+            self.validate_message('m', msg)
+
+    def validate_message(self, phase, msg):
+        if phase == 's':
+            # Validate setup phase client messages
+            return True
+
+        if phase == 'm':
+            # Validate the measurement message
+            return True
+
 
 if __name__ == '__main__':
     # Get the port, or set a default if non is supplied
@@ -52,4 +101,5 @@ if __name__ == '__main__':
     server.listen()
     while 1:
         server.accept()
-        server.get_message()
+        if server.client_socket:
+            server.setup_phase()
