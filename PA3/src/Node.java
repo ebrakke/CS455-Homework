d5edf2ad2df7753c33b4bbb374690336f2ae447c
Erik Brakke <eriksbrakke@gmail.com>
2015-11-19 11:53:36 -0500

    Finished PA3

--- PA3/src/Node.java
+++ PA3/src/Node.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.Arrays;
+
 
 /**
  * This is the class that students need to implement. The code skeleton is provided.
@@ -13,18 +15,145 @@ public class Node {
     int[] lkcost;		/*The link cost between node 0 and other nodes*/
     int[][] costs;  		/*Define distance table*/
     int nodename;               /*Name of this node*/
+
+	int numNodes = 4;
+	String SeparatorLine = "===============================";
     
     /* Class constructor */
-    public Node() { }
+    public Node() {
+		// Need to initialize the arrays
+		costs = new int[numNodes][numNodes];
+		lkcost = new int[numNodes];
+	}
     
     /* students to write the following two routines, and maybe some others */
-    void rtinit(int nodename, int[] initial_lkcost) { }    
+    void rtinit(int nodename, int[] initial_lkcost) {
+		System.out.printf("Initializing Node %d\n", nodename);
+		System.out.println(SeparatorLine);
+		// Get which node this currently is
+		this.nodename = nodename;
+
+		// Start filling in values for the table
+		for (int i = 0; i < numNodes; i++) {
+			for (int j = 0; j < numNodes; j++) {
+				// Fill in the info for directly connected nodes
+				if (i == j) {
+					costs[i][j] = initial_lkcost[j];
+				} else {
+					costs[i][j] = INFINITY;
+				}
+			}
+		}
+		// initialize the cost array
+		lkcost = initial_lkcost;
+		// Send this info to direct neighbors
+		for (int i = 0; i < numNodes; i++) {
+			// Send the info to directly connected nodes
+			if (i == nodename || lkcost[i] == INFINITY) continue;
+			sendRoutingInformation(i);
+		}
+	}
     
-    void rtupdate(Packet rcvdpkt) {  }
-    
-    
+    void rtupdate(Packet rcvdpkt) {
+		System.out.printf("Updating Node %d From sender %d\n", nodename, rcvdpkt.sourceid);
+		System.out.print("Sent ");
+		printlk(rcvdpkt.mincost);
+		System.out.println(SeparatorLine);
+		try {
+			int sender = rcvdpkt.sourceid;
+			for (int i = 0; i < numNodes; i++) {
+				// Update the routing table
+				if (rcvdpkt.mincost[i] == INFINITY) {
+					costs[i][sender] = INFINITY;
+					continue;
+				}
+				costs[i][sender] = rcvdpkt.mincost[i] + lkcost[sender];
+			}
+			updateMinCosts(rcvdpkt.sourceid);
+		}
+		catch (NullPointerException e) {
+			System.out.println("Empty packet was sent");
+			System.exit(1);
+		}
+
+	}
+
+	void updateMinCosts(int sender) {
+		// Useful to see if anything has changed
+		int[] oldLkCost = Arrays.copyOf(lkcost, lkcost.length);
+		int[] poisonArray = new int[numNodes];
+		boolean updated = false;
+		for (int i = 0; i < numNodes; i++) {
+			if (i == nodename) {
+				// This cost is always 0
+				lkcost[i] = 0;
+				continue;
+			}
+			// Otherwise we need to find the least cost
+			int minCost = lkcost[i];
+			if (costs[i][sender] < minCost) {
+				// The sender is now going to be our next hop to i
+				lkcost[i] = costs[i][sender];
+				updated = true;
+				poisonArray[i] = INFINITY;
+			} else {
+				// The sender is not our next hop to node i
+				poisonArray[i] = lkcost[i];
+			}
+		}
+		printdt();
+		if (updated) {
+			System.out.println("Link Costs have been updated!");
+			System.out.print("Old ");
+			printlk(oldLkCost);
+			System.out.print("New ");
+			printlk(lkcost);
+			System.out.println(SeparatorLine);
+			sendPoison(poisonArray, sender);
+		}
+	}
+	void sendRoutingInformation(int destination) {
+		Packet packet = new Packet(nodename, destination, lkcost);
+		System.out.println("Sending information to Node " + destination + " from Node " + nodename);
+		printlk(lkcost);
+		System.out.println(SeparatorLine);
+		NetworkSimulator.tolayer2(packet);
+	}
+
+	void sendPoison(int[] poisonArray, int dest) {
+		Packet packet = new Packet(nodename, dest, poisonArray);
+		System.out.printf("Sending poison packet to Node %d from Node %d\n", dest, nodename);
+		printlk(poisonArray);
+		System.out.println(SeparatorLine);
+		NetworkSimulator.tolayer2(packet);
+	}
+
+	void printlk(int[] lk) {
+		System.out.printf("Link Costs: ");
+		for(int i : lk) {
+			System.out.printf("%d ", i);
+		}
+		System.out.print("\n");
+	}
+
     /* called when cost from the node to linkid changes from current value to newcost*/
-    void linkhandler(int linkid, int newcost) {  }    
+    void linkhandler(int linkid, int newcost) {
+		// Update the cost
+		System.out.printf("\nCost from %d to %d changed to %d\n", nodename, linkid, newcost);
+		costs[linkid][linkid] = newcost;
+		// Now check to see if we need to update anything
+		int minCostToLinkId = INFINITY;
+		int nextHop = INFINITY;
+		for(int i = 0; i < numNodes; i++) {
+			if (costs[linkid][i] < minCostToLinkId) {
+				minCostToLinkId = costs[linkid][i];
+				nextHop = i;
+			}
+		}
+		// Update lkcost and send poison if necessary
+		lkcost[linkid] = INFINITY;
+		updateMinCosts(nextHop);
+	}
 
 
     /* Prints the current costs to reaching other nodes in the network */
